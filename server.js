3f5540c1d820d471de8acbabd015411ef1f85e12
ryap1223 <ryap1223@github.com>
2016-04-28 17:15:57 +0900

    removing dependencies, created new links

--- server.js
+++ server.js
@@ -1,62 +1,24 @@
-var fs = require('fs'),
-    http = require('http'),
-    url = require('url');
+var http = require('http'),
+    fs = require('fs');
     
-
-
-http.createServer(function (req, res) {
-  fs.readFile(__dirname + req.url, function (err,data) {
     
-    console.log('Loaded: ' + req.url);  
+http.createServer(function (request, response) {
+  
+  var link = __dirname;
+  
+  fs.readFile(link + request.url, function (err,data) {
+     console.log(request.url);  
     if (err) {
-      res.writeHead(404);
-      res.end();
-      return;
+    response.writeHead(404);
+    response.end();
+    return;
     }
-    res.writeHead(200);
-    res.end(data);
-    req.url = url.parse(req.url);  
-
-    });
-
+  response.writeHead(200);
+  response.end(data);
+});
+    
 }).listen(8000);
 
 
 //  To-do: make this server static and be able 
 //  to assign specific URLs on specific pages/links.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// http.get('./index.html', function(req, res) {
-//     console.log(`Got response: ${res.statusCode}`);
-//       // consume response body
-//       // fs.readFile(__dirname + req.url, function (err,data) {
-//       //   if (err) {
-//       //     res.writeHead(404);
-//       //     res.end(JSON.stringify(err));
-//       //     return;
-//       //   }
-//       //   res.writeHead(200);
-//       //   res.end(data);
-//       });
-
-
-//       res.resume();
-//     }).on('error', (e) => {
-//       console.log(`Got error: ${e.message}`);
-// });
-//   });
-
-
